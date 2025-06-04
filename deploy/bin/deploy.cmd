bc8945be1c40262c44d7431a429bf6b015895d12
Karel Zavadil <zavadil@incomaker.com>
2025-06-04 20:13:03 +0200

    proxy deployment

--- deploy/bin/deploy.cmd
+++ deploy/bin/deploy.cmd
@@ -1,3 +1,3 @@
 set environment=test
 if not "%~1"=="" set environment=%1
-docker compose run --rm -ti oauth-deploy ansible-playbook -i inventory.yml playbook.yml -l %environment%
+docker compose run --rm -ti okarina-deploy ansible-playbook -i inventory.yml playbook.yml -l %environment%
